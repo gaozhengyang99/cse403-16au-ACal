fdfe454f07dc47297219965183e9b167de538aee
QINRONG LIN <snailin@cs.washington.edu>
2016-11-11 22:48:52 -0800

    unit test

---
+++ app/src/test/java/com/acalendar/acal/EmailmanagerTest.java
@@ -0,0 +1,19 @@
+package com.acalendar.acal;
+
+
+import org.junit.Test;
+
+import java.util.logging.ErrorManager;
+
+public class EmailmanagerTest {
+
+    private ErrorManager email;
+
+    @Test(expected = IllegalArgumentException.class)
+    public void TestSendMess() {
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void onBind() {
+    }
+}
